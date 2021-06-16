77fae8c197750ebc5459c6a9f164f024b852c2ec
Pertwy <johnpatrickperkins@gmail.com>
2021-06-16 08:16:39 +0100

    eronious file removal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,16 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios"
 
-
 import HomePage from "./pages/HomePage"
-import Adduser from "./pages/Adduser"
 import Profile from "./pages/Profile"
 import AddList from "./pages/AddList"
 import ViewBook from "./idPages/ViewBook"
 import User from "./idPages/User"
 import SearchResults from "./pages/SearchResults"
 import AllLists from './idPages/AllLists';
-import Nav2 from "./components/Nav2"
 import Footer from './components/Footer';
 import FollowingBookshelves from './pages/FollowingBookshelves';
-import Test from './pages/Test';
+import LogInSignIn from './pages/LogInSignIn';
 import Members from "./pages/Members"
 import Navbar from "./components/Navbar"
 import ViewList from './idPages/ViewList';
@@ -27,9 +24,6 @@ function App() {
   return (
     <Router>
 
-      {/* <div className={"nav-background"}>
-        <Nav2 />
-      </div> */}
       <div>
         <Navbar />
       </div>
@@ -38,7 +32,6 @@ function App() {
         <div className={"main-container"}>
           <br/>
             <Route path="/" exact component={HomePage} />
-            <Route path="/test" exact component={Adduser} />
             <Route path="/lists" component={AddList} />
             <Route path="/profile" component={Profile} />
             <Route path="/book/:id" component={ViewBook} />
@@ -46,7 +39,7 @@ function App() {
             <Route path="/user/:id" component={User} />
             <Route path="/alllists" component={AllLists} />
             <Route path="/followingBookshelves" component={FollowingBookshelves} />
-            <Route path="/signup" component={Test} />
+            <Route path="/signup" component={LogInSignIn} />
             <Route path="/members" component={Members} />
 
 
--- frontend/src/pages/LogInSignIn.js
+++ frontend/src/pages/LogInSignIn.js
@@ -2,7 +2,7 @@ import React from 'react'
 import SignIn from '../components/SignIn'
 import SignUp from '../components/SignUp'
 
-export default function Test() {
+export default function LogInSignIn() {
     return (
         <div className="container all-back">
             <SignIn/>
